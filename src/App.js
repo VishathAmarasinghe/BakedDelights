91e5b3eed5c789d43ebbee016ecec0ef9b709b87
VishathAmarasinghe <vishathakila2001@gmail.com>
2023-03-01 14:23:21 +0530

    Add stable route paths

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
     <div className="App">
       <Router>
         {/* Add Stable Components Here */}
+        <UpperNotificationBar></UpperNotificationBar>
         <Header />
 
         <Routes>
